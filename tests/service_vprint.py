d0114ed950c96b80aaba1287607cca5dd8a8e36d
Jon Matthew Lucero <matt_lucero@outlook.com>
2025-03-22 21:50:05 -0500

    update test

--- tests/service_vprint.py
+++ tests/service_vprint.py
@@ -1,6 +1,5 @@
-from py_data_viewer import file_output, vprint
+from py_data_viewer import vprint
 from service import generate_mock_response
 
 response = generate_mock_response()
-# vprint(response, var_name="response")
-file_output.output(response, "out/output")
+vprint(response, var_name="response", output="out/response.txt")
